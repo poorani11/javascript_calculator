f223ffea35dcb88a93cdae74280d6a3e58896e63
poorani11 <poorani.mgr@gmail.com>
2016-04-17 17:02:22 +0530

    Added a function to execute the backspace functionality

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ function answer(){
     x=eval(x);
     box.value=x;
 }
+
+function backspace(){
+    number=box.value;
+    len=number.length-1;
+    newNumber=number.substring(0,len);
+    box.value=newNumber;
+}
