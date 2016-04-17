0ba4141b87e0f60feb71fd6879df0270d2262a79
poorani11 <poorani.mgr@gmail.com>
2016-04-17 16:57:15 +0530

    Added a js file for the calculator

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var box=document.getElementById('display');
+function addtoscreen(x){
+    box.value +=x;
 
+    if(x =='C'){
+        box.value ='';
+    }
+}
+
+function answer(){
+    x=box.value;
+    x=eval(x);
+    box.value=x;
+}
